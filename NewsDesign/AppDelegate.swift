e3ec601ab6affb993e1f73cfc2b5d685c9d32330
samira273 <samiramarassy@gmail.com>
2019-10-03 12:51:31 +0200

    comment done

--- NewsDesign/AppDelegate.swift
+++ NewsDesign/AppDelegate.swift
@@ -12,10 +12,20 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var navController: UINavigationController?
+    var viewController: ViewController?
+
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        window = UIWindow(frame:UIScreen.main.bounds)
+        viewController = ViewController(nibName: "ViewController", bundle: nil)
+        navController = UINavigationController(rootViewController: viewController!)
+        window?.rootViewController = navController
+        window?.makeKeyAndVisible()
+       
         return true
     }
 
